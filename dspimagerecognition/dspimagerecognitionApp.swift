dfcb411f7b10ccc0b182f8d32e33edb5ff12af61
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2024-01-16 14:41:47 +0200

    Initial Commit

---
+++ dspimagerecognition/dspimagerecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  dspimagerecognitionApp.swift
+//  dspimagerecognition
+//
+//  Created by Matei Crăiniceanu on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct dspimagerecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
